bb2ff1f421f3873686c927b42ee9ce9409d4ced3
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-02-27 00:32:15 +0600

    Making a widget

---
+++ lib/widgets/bottom_nav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbar extends StatefulWidget {
+  const BottomNavbar({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavbarState createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
