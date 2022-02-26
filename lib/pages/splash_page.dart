873bcf36c25f6e4ffa705037576128d5925d3e15
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-02-27 00:03:00 +0600

    complete splash page design

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -33,13 +33,54 @@ class _SplashPageState extends State<SplashPage> {
               style: TextStyle(
                   fontSize: 18,
                   color: Colors.grey,
-                  fontWeight: FontWeight.w600),
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.8),
             ),
             SizedBox(
               height: 450,
               width: 450,
               child: Image.asset('assets/images/Asset1.png'),
-            )
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 80.0, vertical: 12.0),
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Text(
+                  'Sign in',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Create an account',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(.7),
+                    letterSpacing: 1),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                'Forget password?',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.red.withOpacity(.5),
+                    letterSpacing: 1),
+              ),
+            ),
           ],
         ),
       ),
