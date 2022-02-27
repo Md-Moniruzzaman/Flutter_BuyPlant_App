bc1cf27ee27772bd58dbfe41be68e1ff8b5f140a
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-02-27 22:33:38 +0600

    Create a Search Fiesld

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,6 +10,113 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        leadingWidth: 40,
+        leading: TextButton(
+          onPressed: () {},
+          child: Image.asset("assets/icons/menu.png"),
+        ),
+        actions: [
+          Container(
+            height: 40,
+            width: 40,
+            margin: const EdgeInsets.only(left: 20, right: 10, bottom: 5),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.green.withOpacity(.5),
+                  blurRadius: 10,
+                  offset: const Offset(0, 0),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/pro.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 45,
+                    width: 300,
+                    margin:
+                        const EdgeInsets.only(left: 20, right: 10, bottom: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.green),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.green.withOpacity(.5),
+                          blurRadius: 10,
+                          offset: const Offset(0, 0),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      children: [
+                        const SizedBox(
+                          height: 45,
+                          width: 250,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: '   Search',
+                            ),
+                          ),
+                        ),
+                        Image.asset(
+                          'assets/icons/search.png',
+                          height: 25,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    width: 45,
+                    margin:
+                        const EdgeInsets.only(left: 20, right: 10, bottom: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      // border: Border.all(color: Colors.green),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.green.withOpacity(.5),
+                          blurRadius: 10,
+                          offset: const Offset(0, 0),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Image.asset(
+                      'assets/icons/adjust.png',
+                      height: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
