0788a38065366c5eade5d3e4074f83c76f58f89b
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2022-02-27 22:32:16 +0600

    Create a Category model

---
+++ lib/models/catogories_model.dart
@@ -0,0 +1,12 @@
+class Category {
+  final int id;
+  final String name;
+  Category({required this.id, required this.name});
+}
+
+List<Category> ctegories = [
+  Category(id: 0, name: 'All'),
+  Category(id: 1, name: 'Outdoor'),
+  Category(id: 2, name: 'Indoor'),
+  Category(id: 3, name: 'Office'),
+];
